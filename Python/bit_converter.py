a9f2cdceb5c0108cfb08367a3407876cfd858bc8
Luiz Souza <fongoses@gmail.com>
2013-12-13 11:12:52 -0200

    Trabalho final separado em outro repositório

---
+++ Python/bit_converter.py
@@ -0,0 +1,20 @@
+def tobits(s):
+    result = []
+    for c in s:
+        bits = bin(ord(c))[2:]
+        bits = '00000000'[len(bits):] + bits
+        result.extend([int(b) for b in bits])
+    return result
+
+def frombits(bits):
+    chars = []
+    for b in range(len(bits) / 8):
+        byte = bits[b*8:(b+1)*8]
+        chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
+    return ''.join(chars)
+
+texto = 'teste'
+bits = tobits(texto)
+print texto
+print bits
+print frombits(bits)
